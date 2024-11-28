554283a076b6789677e8517db8ba088368faef0c
NE920 <2719751874@qq.com>
2024-11-28 20:52:15 +0800

    第二次提交

---
+++ 作业4/lv1.js
@@ -0,0 +1,10 @@
+function sum(num1,num2){
+    return num1+num2;
+}
+let sum2 = function(num1,num2){
+    return num1+num2;
+}
+let sum3 = (num1,num2)=>{
+    return num1+num2;
+}
+console.log(sum(1,2));
